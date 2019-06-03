9c6829bbc8a98bc1d343049926a9d80b6be2e20d
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-06-03 23:47:07 +0900

    Vue実践入門 2.9まで

---
+++ tutorial/app.js
@@ -0,0 +1,31 @@
+var items = [
+    {
+        name: '鉛筆',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: 'ノート',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: '消しゴム',
+        price: 500,
+        quantity: 0
+    }
+]
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    }
+})
+vm.$watch(function () {
+    return this.items[0].quantity
+}, function (quantity) {
+    console.log(quantity)
+})
+
+window.vm = vm
