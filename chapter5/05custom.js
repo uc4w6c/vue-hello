4c26e07568c350a27181b79e1e2a74ed4227370e
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-06-11 00:08:53 +0900

    カスタムディレクティブを作成

--- chapter5/05custom.js
+++ chapter5/05custom.js
@@ -1,11 +1,20 @@
 Vue.directive('fallback-image', {
-    bind: function(el) {
+    bind: function(el, binding) {
+        console.log('bind', binding)
         el.addEventListener('error', function() {
             el.src = 'https://dummyimage.com/10×10/000/ffffff.png&text=no+image'
         })
+    },
+    update: function(el, binding) {
+        console.log('update', binding)
     }
 })
 
-new Vue({
+var vm = new Vue({
     el: '#app',
+    data: function() {
+        return {
+            altText: 'log'
+        }
+    }
 })
