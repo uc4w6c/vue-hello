10feed448215d27aad520b8fc2eb8ca9953015c5
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-06-13 22:25:37 +0900

    第6章単一コンポーネント

---
+++ chapter6/bar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bar">
+        <h1 class="header">Barコンポーネント</h1>
+        <p>これはBarコンポーネントです。</p>
+        <foo/>
+    </div>
+</template>
+
+<script>
+import Foo from './foo'
+export default {
+    components: {
+        Foo
+    }
+}
+</script>
+<style>
+p { text-decoration: underline; }
+</style>
+
+<style scoped>
+.bar {
+    border: solid 1px red;
+    margin: 4px;
+    padding: 4px;
+}
+.header { font-size: 125%; }
+</style>
